93e5f7d4e31db85183c70f9ed6b8d5eda05881ed
AravArora05 <81041030+AravArora05@users.noreply.github.com>
2024-06-08 23:38:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,88 @@
-$(document).ready(function () {
-  const colors = [
-    "#FF5733",
-    "#33FF57",
-    "#3357FF",
-    "#FF33A1",
-    "#FFBD33",
-    "#8D33FF",
-    "#33FFF6",
-    "#F633FF",
-    "#33FFBD",
-    "#FFC433",
-    "#FF5733",
-    "#33FFB5",
-    "#FF33E6",
-    "#57FF33",
-    "#3375FF",
-    "#FF8D33",
-    "#33FFFC",
-    "#B533FF",
-    "#FF3333",
-    "#33FF8D"
-  ];
+#quote-box {
+  background-color: white;
+  margin: auto;
+  width: 60%;
+  max-width: 400px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 15px;
+  padding: 15px;
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-size: 20px;
+  /**
+  The border radius property in the regular Bootstrap was not something that I wanted to use.
+Consequently, I decided to use this
+ 
+*/
+}
 
-  const quotesData = [
-    {
-      quote: "The only way to prove that you’re a good sport is to lose.",
-      author: "Ernie Banks"
-    },
-    {
-      quote: "Winning isn’t everything, but wanting to win is.",
-      author: "Vince Lombardi"
-    },
-    {
-      quote: "You miss 100% of the shots you don’t take.",
-      author: "Wayne Gretzky"
-    },
-    {
-      quote: "The harder the battle, the sweeter the victory.",
-      author: "Les Brown"
-    },
-    {
-      quote: "Champions keep playing until they get it right.",
-      author: "Billie Jean King"
-    },
-    {
-      quote: "It’s not whether you get knocked down; it’s whether you get up.",
-      author: "Vince Lombardi"
-    },
-    {
-      quote:
-        "Never let the fear of striking out keep you from playing the game.",
-      author: "Babe Ruth"
-    },
-    {
-      quote: "Success is where preparation and opportunity meet.",
-      author: "Bobby Unser"
-    },
-    {
-      quote: "Pain is temporary. Quitting lasts forever.",
-      author: "Lance Armstrong"
-    },
-    {
-      quote:
-        "You can’t put a limit on anything. The more you dream, the farther you get.",
-      author: "Michael Phelps"
-    }
-  ];
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 20px;
+}
 
-  let randomQuote = quotesData[0].quote;
-  let randomQuoteAuthor = quotesData[0].author;
+button,
+.btn {
+  background-color: #007bff;
+  border: none;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+}
 
-  function changeQuote() {
-    let randomQuoteIndex = Math.floor(Math.random() * quotesData.length);
-    randomQuoteAuthor = quotesData[randomQuoteIndex].author;
-    randomQuote = quotesData[randomQuoteIndex].quote;
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    $("#text").fadeOut(1000);
-    $("#author").fadeOut(1000);
-    $("body").css({
-      "background-color": randomColor,
-      transition: "background-color 2s"
-    });
+button:hover,
+.btn:hover {
+  background-color: #0056b3;
+}
 
-    setTimeout(function () {
-      $("#text h1").text(randomQuote);
-      $("#author h4").text(randomQuoteAuthor);
-      $("#text").css({ color: randomColor });
-      $("#author").css({ color: randomColor });
-      $("#text").fadeIn(1000);
-      $("#author").fadeIn(1000);
-    }, 1000);
-  }
+#quotes1,
+#quotes2 {
+  position: relative;
+  top: -7px;
+  size: 25px;
+}
+#quotes1 {
+  margin-left: 4px;
+}
+#quotes2 {
+  margin-right: 4px;
+}
+#author {
+  font-family: Times New Roman;
+}
 
-  $("#tweet-quote").on("click", function () {
-    $(this).attr(
-      "href",
-      "https://twitter.com/intent/tweet?hashtags=CodingCoolThings&related=freecodecamp&text=" +
-        encodeURIComponent(randomQuote + " " + randomQuoteAuthor)
-    );
-  });
+#text {
+  padding: 5px;
+}
 
-  $("#tumblr-quote").on("click", function () {
-    window.open(
-      "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=CodingCoolThings,freecodecamp&caption=" +
-        encodeURIComponent(randomQuoteAuthor) +
-        "&content=" +
-        encodeURIComponent(randomQuote) +
-        "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
-    );
-  });
+#author {
+  margin-top: -5px;
+}
+#author h4,
+#author span {
+  margin-top: -10px;
+  font-size: 15px;
+}
+#text h1 {
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-size: 30px;
+  margin-left: 4px;
+  margin-right: 4px;
+  padding-top: 5px;
+}
+/**
+body {
+    background-color: green;
+}
+*/
 
-  $("#new-quote").on("click", changeQuote);
-});
+h1,
+h4 {
+  display: inline;
+}
